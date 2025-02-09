5d8541e8f00b2d6cf1b45fdbe0e750c6296652f0
Yogita Goyal <yogitagoel2006@gmail.com>
2025-02-10 02:08:13 +0530

    Create script1.js
    homepage javascript file

---
+++ script1.js
@@ -0,0 +1,8 @@
+let mode1=document.getElementById("pwf");
+let mode2=document.getElementById("pwc");
+mode1.addEventListener("click",()=>{
+    window.location.href = "pwf.html";
+})
+mode2.addEventListener("click",()=>{
+    window.location.href = "pwc.html";
+})
